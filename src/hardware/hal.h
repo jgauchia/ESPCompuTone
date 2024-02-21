afea1f9ee68d359b9979718c1384b0d247a1e873
jgauchia <jgauchia@gmx.es>
2024-02-21 23:29:10 +0100

    Audio bypass test

--- src/hardware/hal.h
+++ src/hardware/hal.h
@@ -10,7 +10,17 @@
  * @brief SD pin definition
  *
  */
-  #define SD_CS 4
-  #define SD_MISO 19 
-  #define SD_MOSI 23 
-  #define SD_CLK 12
+#define SD_CS 4
+#define SD_MISO 19
+#define SD_MOSI 23
+#define SD_CLK 12
+
+/**
+ * @brief I2S pin definition
+ * 
+ */
+#define I2S_BCK 26
+#define I2S_LRC 33
+#define I2S_DIN 35
+#define I2S_DOUT 25
+#define I2S_MCK 0
---
+++ src/hardware/sdcard.h
@@ -0,0 +1,44 @@
+/**
+ * @file sdcard.h
+ * @author Jordi Gauchia
+ * @brief SD Card definition and functions
+ * @version 0.1
+ * @date 2024-02-21
+ */
+
+#include <FS.h>
+#include <SD.h>
+
+
+SPIClass spiSD = SPIClass(VSPI);
+uint32_t sd_freq = 40000000;
+
+/**
+ * @brief SD Card init
+ *
+ */
+void init_sd()
+{
+  spiSD.begin(SD_CLK, SD_MISO, SD_MOSI, SD_CS);
+  pinMode(SD_CS,OUTPUT);
+  digitalWrite(SD_CS,LOW);
+  if (!SD.begin(SD_CS, spiSD, sd_freq))
+  {
+    log_i("SD Card Mount Failed");
+    return;
+  }
+  else
+   log_i("SD Card Mounted");
+}
+
+/**
+ * @brief SPIFFS Init
+ *
+ */
+void init_SPIFFS()
+{
+  if (!SPIFFS.begin(true))
+    log_i("SPIFFS Mount Failed");
+  else
+    log_i("SPIFFS Mounted");
+}
